3f94635e1bcdba8b3c535bccddff2d982cde8ef4
appygautam <81364007+appygautam@users.noreply.github.com>
2021-12-23 16:46:43 +0530

    Add files via upload

--- reactappnew/src/components/Navbar/index.js
+++ reactappnew/src/components/Navbar/index.js
@@ -1,1 +1,51 @@
+import React from 'react';
+import logo from 'file:///C:/Users/gulsh/reactappnew/src/logo.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+      <NavLink to='/home' className="logo" activeStyle><img src={logo} alt="logo" className='app-logo' /></NavLink>
+        <Bars className="side-bar" />
 
+
+          <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+            </NavLink>
+          <NavLink to='/events' activeStyle>
+            Events
+          </NavLink>
+          <NavLink to='/annual' activeStyle>
+            Annual Report
+          </NavLink>
+          <NavLink to='/team' activeStyle>
+            Teams
+          </NavLink>
+          <NavLink to='/blogs' activeStyle>
+            Blogs
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Sign Up
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
